e1bff895925a4ad5301ca20317005ab904641f70
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-08-11 23:35:34 +0530

    COMMAND LINE ARGUMENT: Done

---
+++ 3-Asimilating-C++/CommandLine.cpp
@@ -0,0 +1,18 @@
+/*
+    Program to Illustrate Command Line Arguments
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int sum = 0;
+
+    for(int i = 1; i < argc; i++) {
+        sum += atoi(argv[i]);
+    }
+
+    cout << "Sum of given number is : " << sum;
+
+    return 0;
+}
