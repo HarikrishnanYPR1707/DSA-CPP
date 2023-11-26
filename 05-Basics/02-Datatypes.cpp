bf3ef5707b2a372b5ae4a8803e0a6b8ae1f45725
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-11-26 22:11:37 +0530

    Data types done

---
+++ 05-Basics/02-Datatypes.cpp
@@ -0,0 +1,35 @@
+/*
+Each data type has a different size. You’ve studied 5 different data types 
+and the sizes of the data types:
+
+Integer: 4 bytes
+Long: 8 bytes
+Float: 4 bytes
+Double: 8 bytes
+Character: 1 byte
+
+You’re given a data type. Print its size in bytes.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int dataTypes(string type) {
+	if(type == "Integer")
+		cout << "4";
+	else if (type == "Long")
+		cout << "8";
+	else if (type == "Float")
+		cout << "4";
+	else if (type == "Double")
+		cout << "8";
+	else if (type == "Character")
+		cout << "1";
+}
+
+int main() {
+    string str;
+    getline(cin, str);
+    dataTypes(str);
+    return 0;
+}
