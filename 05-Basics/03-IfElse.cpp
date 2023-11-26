3cb5057351a33477f4dee227f69b706f149f2ac5
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-11-26 22:15:34 +0530

    If-Else done

---
+++ 05-Basics/03-IfElse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+string compareIfElse(int a, int b) {
+  if (a < b)
+    return "smaller";
+  else if (a > b)
+    return "greater";
+  else
+    return "equal";
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << compareIfElse(a, b);
+    return 0;
+}
