04c1fa4b64384cc602551408fe64204ce2885060
Harikrishnan17 <hyprgd2002@gmail.com>
2024-05-17 22:04:03 +0530

    Added Changes

--- 05-Basics/15-reverseBits.cpp
+++ 05-Basics/15-reverseBits.cpp
@@ -1,14 +1,3 @@
 // #There is a song concert going to happend in the city.The price of
 // #each ticket is equal to the number obtained after reversing the bits
 // #of a given 32 bits usigned integer 'n'.
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main()
-{
-    cout << "hello wrold!!!";
-
-    return 0;
-}
